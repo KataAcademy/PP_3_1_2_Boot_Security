d5b5bb324e7e0b3c982af2a9b303f600292e0d7d
MishaSkl <112400829+MishaSkl@users.noreply.github.com>
2022-11-29 07:51:21 +0300

    deleteButton

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,10 +1,13 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.Admin;
+import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
@@ -21,10 +24,14 @@ public class UserController {
     }
 
     @GetMapping("")
-    public String userPage(Model model, Principal principal) {
+    public String userPage(@AuthenticationPrincipal User user, Model model, Principal principal) {
+        Admin admin = new Admin(principal.getName());
+        System.out.println(principal + "++++++++++ +++++++++++++++++++");
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("admin", admin);
         Long id = userService.getUserByUsername(principal.getName()).getId();
         model.addAttribute("user", userService.getUserById(id));
-        return "user";
+        return "admin/userPage";
     }
 
 }
