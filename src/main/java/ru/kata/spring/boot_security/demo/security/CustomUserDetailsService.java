7195963913d1ff714f41db38da1c3160de4da5df
Mafinor <i.mafinor@gmail.com>
2023-03-28 02:53:04 +0600

    remove transactional, remove MvcConfig

--- src/main/java/ru/kata/spring/boot_security/demo/security/CustomUserDetailsService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/security/CustomUserDetailsService.java
@@ -16,7 +16,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userDao.findUserByEmailFetchRoles(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Incorrect username or password"));
