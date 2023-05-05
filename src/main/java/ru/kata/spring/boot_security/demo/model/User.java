7bfd043c1a8980b23447c64486557f7f25bd7e3f
Salavaat_ <salavath94@gmail.com>
2023-05-05 15:33:06 +0400

    changes

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -5,9 +5,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 import java.util.Collection;
 
 @Entity
