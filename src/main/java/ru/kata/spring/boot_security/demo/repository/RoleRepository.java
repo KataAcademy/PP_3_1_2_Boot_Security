11059c2ca9e4e59e2f2d1670bcd0a15d3f6250da
Salavaat <124880043+Salavaat@users.noreply.github.com>
2023-05-10 10:39:56 +0400

    Delete src directory

--- src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.kata.spring.boot_security.demo.model.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
