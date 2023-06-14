95c22e69d9973a14eff6b8422e7344155cae2906
Chilloutsaur <110351523+Chilloutsaur@users.noreply.github.com>
2023-06-14 16:23:07 +0300

    Complete 3.1.3

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private UserService userService;
+
     private final SuccessUserHandler successUserHandler;
 
     public WebSecurityConfig(SuccessUserHandler successUserHandler) {
@@ -51,52 +52,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//     аутентификация inMemory
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("user")
-//                        .roles("USER")
-//                        .build();
-//
-//        UserDetails admin =
-//                User.withDefaultPasswordEncoder()
-//                        .username("admin")
-//                        .password("admin")
-//                        .roles("ADMIN","USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-
-
-
 }
-
-//    private final SuccessUserHandler successUserHandler;
-//
-//    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
-//        this.successUserHandler = successUserHandler;
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/", "/index").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().successHandler(successUserHandler)
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-//    }
-//
-//
-//}
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entities.Role;
@@ -26,7 +27,6 @@ public class UserService implements UserDetailsService {
     @Autowired
     private RoleService roleService;
 
-    //Для создания UserDetails используется интерфейс UserDetailsService, с единственным методом:
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = findByUsername(username);
@@ -35,28 +35,21 @@ public class UserService implements UserDetailsService {
         }
         return user;
     }
-//    public User(String username, String password, String email, Collection<Role> roles) {
-//        this.username = username;
-//        this.password = password;
-//        this.email = email;
-//        this.roles = roles;
-//    }
 
     public void init() {
         roleService.saveRole(new Role("ROLE_ADMIN"));
         roleService.saveRole(new Role("ROLE_USER"));
-        userRepository.save(new User("userAdmin", "$2a$12$8ba3pDR6kEhzdQd8yScQaetFUdsJtJx9hFmEtOcpOV7PNwaJRMLu2","user1Admin@gmail.com",
-                roleService.getRoleById(2L), roleService.getRoleById(1L)));
-        userRepository.save(new User("user", "$2a$12$8ba3pDR6kEhzdQd8yScQaetFUdsJtJx9hFmEtOcpOV7PNwaJRMLu2","user1@gmail.com",
-                roleService.getRoleById(2L)));
-        userRepository.save(new User("admin","$2a$12$8ba3pDR6kEhzdQd8yScQaetFUdsJtJx9hFmEtOcpOV7PNwaJRMLu2","admin1@gmail.com",
-                roleService.getRoleById(1L)));
+        userRepository.save(new User("userAdmin", "$2a$12$8ba3pDR6kEhzdQd8yScQaetFUdsJtJx9hFmEtOcpOV7PNwaJRMLu2", "user1Admin@gmail.com", roleService.getRoleById(2L), roleService.getRoleById(1L)));
+        userRepository.save(new User("user", "$2a$12$8ba3pDR6kEhzdQd8yScQaetFUdsJtJx9hFmEtOcpOV7PNwaJRMLu2", "user1@gmail.com", roleService.getRoleById(2L)));
+        userRepository.save(new User("admin", "$2a$12$8ba3pDR6kEhzdQd8yScQaetFUdsJtJx9hFmEtOcpOV7PNwaJRMLu2", "admin1@gmail.com", roleService.getRoleById(1L)));
     }
 
-
+    @Transactional(readOnly = true)
     public User findByUsername(String username) {
         return userRepository.findByUsername(username).get();
     }
+
+    @Transactional(readOnly = true)
     public User findUserById(Long userId) {
         Optional<User> userFromDb = userRepository.findById(userId);
         if (userFromDb.isEmpty()) {
@@ -65,6 +58,7 @@ public class UserService implements UserDetailsService {
         return userFromDb.get();
     }
 
+    @Transactional(readOnly = true)
     public List<User> allUsers() {
         return userRepository.findUsers();
     }
@@ -76,7 +70,6 @@ public class UserService implements UserDetailsService {
     public void deleteUser(Long userId) {
         userRepository.deleteUserById(userId);
     }
-
 
 
 }
