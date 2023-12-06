da7da4beeb53bcad397cab0b502ed7d9eb9289fd
Max <demonbleach94@gmail.com>
2023-12-06 21:44:42 +0300

    feat: delete dao and create repository

---
+++ src/main/java/ru/summer/spring/boot_security/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package ru.summer.spring.boot_security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.summer.spring.boot_security.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
