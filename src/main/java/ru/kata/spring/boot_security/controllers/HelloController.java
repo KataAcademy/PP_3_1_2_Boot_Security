8b706bb8ca37b57c67e8c88fd3e8c5b213a2d134
Senz One <sem63sam@yandex.ru>
2024-02-18 01:33:47 +0400

    create security without provaider

--- src/main/java/ru/kata/spring/boot_security/controllers/HelloController.java
+++ src/main/java/ru/kata/spring/boot_security/controllers/HelloController.java
@@ -1,7 +1,10 @@
 package ru.kata.spring.boot_security.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.security.PersonDetails;
 
 @Controller
 public class HelloController {
@@ -9,5 +12,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
         return "hello";
+    }
+
+
+    @GetMapping("user")
+    public String showUserInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPersson());
+        return "user";
     }
 }
