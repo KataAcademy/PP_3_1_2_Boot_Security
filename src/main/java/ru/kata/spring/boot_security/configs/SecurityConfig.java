51714deaacce44e59a5296e4d0eb550d8a9337f2
Senz One <sem63sam@yandex.ru>
2024-02-18 16:10:30 +0400

    added csrf

--- src/main/java/ru/kata/spring/boot_security/configs/SecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/configs/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/", "/error", "/auth/registration").permitAll()
                 .anyRequest().authenticated()
